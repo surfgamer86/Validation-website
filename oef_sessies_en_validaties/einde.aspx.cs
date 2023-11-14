b024ecf2cb5a38276c67f8bcc73a121e4e4d80b0
Ilyaan <ilyaan.ali@hotmail.com>
2023-11-14 10:30:48 +0100

    Added c# user

--- oef_sessies_en_validaties/einde.aspx.cs
+++ oef_sessies_en_validaties/einde.aspx.cs
@@ -5,10 +5,13 @@ namespace oef_sessies_en_validaties
 
     public partial class einde : System.Web.UI.Page
     {
-        /* Gebruiker Session = new Gebruiker();*/
+        
+         
         String Nieuws;
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            _Nieuwegebruiker = new Gebruiker(Session["email"].ToString(), Session["passwoord"].ToString(), Session["provider"].ToString(),Convert.ToBoolean(Session["nieuws"]));
             emailLabel.Text = Convert.ToString(Session["email"]) + "@" + Convert.ToString(Session["provider"]);
             wachtwoordLabel.Text = Convert.ToString(Session["password"]);
             if (Convert.ToBoolean(Session["nieuws"] = true))
